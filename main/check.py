b95707e3521531a1b12e78f5b9bdf818dba923fe
admin <394286561@qq.com>
2019-06-20 11:08:21 +0800

    Initial commit

---
+++ main/check.py
@@ -0,0 +1,132 @@
+
+import logging
+import random
+import os
+import shutil
+import sys
+logger = logging.getLogger("sample")
+# 总的图片目录txt文件位置
+txt_all = 'train.txt'
+
+# 分出图片的路径
+person_path = "data/everybody/"
+# 源文件路径
+src_path = '/src'
+# 正确文件路径
+good_path = '/good'
+# 错误文件路径
+bad_path = '/bad'
+
+def init_logger():
+    logging.basicConfig(
+        format='%(asctime)s : %(levelname)s : %(message)s',
+        level=logging.DEBUG,
+        handlers=[logging.StreamHandler()])
+
+# 得到用户目前的状态
+def get_status(user_name):
+    user_path = os.path.join('%s%s%s' % (person_path,user_name,src_path))
+    if not os.path.isdir(user_path):
+        return 0
+    else:
+        return 1
+
+
+# 如果正确的图片移到good下，错误的图片移到bad下
+def move_img_good_or_bad(user_name,type,img_path):
+    user_path = os.path.join('%s%s%s' % (person_path, user_name, src_path))
+    to_good_path =  os.path.join('%s%s%s' % (person_path, user_name, good_path))
+    to_bad_path =  os.path.join('%s%s%s' % (person_path, user_name, bad_path))
+    if type=='good':
+        if not os.path.isdir(to_good_path):
+            os.mkdir(to_good_path)
+        moveFileto(img_path,to_good_path)
+    else:
+        if not os.path.isdir(to_bad_path):
+            os.mkdir(to_bad_path)
+        moveFileto(img_path,to_bad_path)
+
+    # 修改src下的txt文件
+    txt_name = os.path.join('%s%s' % (user_name, '.txt'))
+    txt_path = os.path.join('%s%s%s' % (user_path, '/', txt_name))
+    file = open(txt_path, 'r')
+    lines = file.readlines()  # 读取所有行
+    label = lines[0]
+    with open(txt_path, "w", encoding="utf-8") as f_w:
+        for line in lines:
+            if lines[0] in line:
+                continue
+            f_w.write(line)
+    # 写入good或者bad文件夹中的txt
+    write_label_to_newfile(label,user_name,type)
+
+# 将label写入该用户的good文件夹或者bad文件夹
+def write_label_to_newfile(label,user_name,type):
+    user_txt_name = os.path.join('%s%s' % (user_name, ".txt"))
+    # 源文件路径
+    new_txt_path = ''
+    if type == 'good':
+        new_txt_path = os.path.join('%s%s%s%s%s%s' % (person_path, user_name ,"/",good_path,"/",user_txt_name))
+    else:
+        new_txt_path = os.path.join('%s%s%s%s%s%s' % (person_path, user_name ,"/",bad_path,"/",user_txt_name))
+    logger.info("将label加入到%s",new_txt_path)
+    fopen = open(new_txt_path, 'a+')
+    write_txt = label
+    fopen.write('%s%s' % (write_txt, os.linesep))
+    fopen.close()
+
+
+# 用户拿src下的原图片和标签
+def get_img_in_src(user_name):
+    user_path = os.path.join('%s%s%s' % (person_path,user_name,src_path))
+    txt_name = os.path.join('%s%s' % (user_name,'.txt'))
+    txt_path = os.path.join('%s%s%s' % (user_path,'/',txt_name))
+    img_txt = open(txt_path, 'r')
+    lines = img_txt.readlines()  # 读取所有行
+    if len(lines) <= 0 :
+        return '',''
+    first_line = lines[0].split()  # 取第一行
+    img_name = first_line[0]
+    label = first_line[1]
+    # 图片
+    img_path = os.path.join('%s%s%s' % (user_path,'/',img_name))
+    return img_path,label
+
+
+
+
+# 将该图片和label写入该用户的文件夹
+def write_img_label_to_newfile(img_path,label,user_name,img_name):
+    img_path = os.path.join('%s%s' % ("../", img_path))
+    new_path = os.path.join('%s%s' % (person_path, user_name))
+    logger.info("邮箱前缀为" + user_name + "的用户的文件路径为" + new_path)
+    if not os.path.isdir(new_path):
+        os.mkdir(new_path)
+    # 源文件路径
+    img_src = os.path.join('%s%s' % (new_path, src_path))
+    if not os.path.isdir(img_src):
+        os.mkdir(img_src)
+    logger.info("将图片%r移动到%r",img_path,img_src)
+    moveFileto(img_path,img_src)
+    # 用户对应的txt
+    user_txt_name = os.path.join('%s%s' % (user_name,".txt"))
+    user_txt = os.path.join('%s%s%s%s' % (new_path, src_path ,"/",user_txt_name))
+    fopen = open(user_txt, 'a+')
+    write_txt = img_name + " " + label
+    fopen.write('%s%s' % (write_txt, os.linesep))
+    fopen.close()
+
+def moveFileto(sourceDir,  targetDir):
+    shutil.move(sourceDir,  targetDir)
+
+
+#
+if __name__ == '__main__':
+
+    import argparse
+
+    init_logger()
+    # 邮箱前缀
+    user_name = 'hengyang4'
+    img_path = get_img_in_src(user_name)
+
---
+++ web/server.py
@@ -0,0 +1,61 @@
+#-*- coding:utf-8 -*-
+from flask import Flask,jsonify,request,render_template
+import base64,cv2,numpy as np,logging
+from threading import current_thread
+import os,sys
+from main import sample
+from main import check
+import json
+app = Flask(__name__,root_path="web")
+person_img_num = 0
+
+logger = logging.getLogger("WebServer")
+
+@app.route("/")
+def index():
+    return render_template('index.html',version="version")
+
+
+def return_img_stream(img_local_path):
+    """
+    工具函数:
+    获取本地图片流
+    :param img_local_path:文件单张图片的本地绝对路径
+    :return: 图片流
+    """
+    import base64
+    img_stream = ''
+    with open(img_local_path, 'rb') as img_f:
+        img_stream = img_f.read()
+        img_stream = base64.b64encode(img_stream)
+    return str(img_stream,'utf-8')
+
+# 开始任务
+@app.route('/start',methods=['GET'])
+def start():
+    username = request.args['username']
+    if check.get_status(username) == 0:
+        # 没有领任务
+        sample.get_img_by_person(person_img_num,username)
+
+    # 已经有任务
+    img_path,label = check.get_img_in_src(username)
+    if img_path == '' or label == '':
+        return ''
+    img_stream = return_img_stream(img_path)
+    return jsonify({'img_stream': str(img_stream),'img_path':img_path,'label':label})
+
+# 图片判定
+@app.route('/check',methods=['POST'])
+def img_check():
+    username = request.json.get('username')
+    type = request.json.get('type')
+    img_path = request.json.get('img_path')
+    check.move_img_good_or_bad(username,type,img_path)
+    return "ok"
+
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 1:
+        person_img_num = int(sys.argv[1])
+    app.run(debug=True, port=8080)
